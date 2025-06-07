8d249f4783052ede4dd2953cdfba0193b4b5bf39
Elina Hailaijiao <elinaxiaomei@yahoo.com>
2025-06-07 14:52:39 -0700

    Add accordion blocks

--- script.js
+++ script.js
@@ -11,5 +11,12 @@ greeting("Elina");
 function saveName() {
     let newName = document.getElementById("greeter").value; // text field value from the user
     document.getElementById('username').textContent = newName;
-    document.getElementById("greeter").value = "";
+    document.getElementById("greeter").value = '';
 }
+
+document.querySelectorAll('.accordion-header').forEach(function (button){
+    button.addEventListener('click', function() {
+        let content = this.nextElementSibling;
+       content.style.display = (content.style.display === 'block') ? 'none' : 'block';
+    });
+});
